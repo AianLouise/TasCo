3dbb30c8739e5d1e2219307c0f1478d8cf849c64
AianLouise <aianlouisealfaro03@gmail.com>
2023-10-17 12:39:11 +0800

    Register page done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -55,24 +55,26 @@
 
         <!-- Confirm Password -->
         <div class="relative">
-            <div class="form-control">
+            <div class="">
                 <x-text-input id="password_confirmation" placeholder="Confirm Password" class="add-pad i-1 block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required autocomplete="new-password" />
                 <i class="fas fa-lock"></i>
             </div>
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-1" />
         </div>
 
+        @if (Route::has('login'))
+            <div class="text-left mt-2"> <!-- Change text-center to text-left, and reduce mt value -->
+                <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __("Already have an account?") }} <span>Log In</span>
+                </a>
+            </div>
+        @endif
+        
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="flex items-center justify-center "> <!-- Center the button horizontally and vertically -->
+            <button class="submit">Register</button>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -239,7 +239,7 @@
 
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             {{-- {{print_r(URL(""))}} --}}
-            <div class=" slot w-full sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class=" slot w-full sm:max-w-lg  px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>  
         </div>
