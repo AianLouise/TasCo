5293128210184cbcf8ec1bd5c2d79d95f1e25088
AianLouise <aianlouisealfaro03@gmail.com>
2023-11-15 21:31:51 +0800

    added homepage

--- resources/views/admin/admin-services.blade.php
+++ resources/views/admin/admin-services.blade.php
@@ -79,7 +79,7 @@
                             </th>
                             <th scope="col"
                               class="px-6 py-3 text-left text-xs font-medium2 text-gray-800 uppercase tracking-wider">
-                              Author
+                              Employer
                             </th>
                             <th scope="col"
                               class="px-6 py-3 text-left text-xs font-medium2 text-gray-800 uppercase tracking-wider">
