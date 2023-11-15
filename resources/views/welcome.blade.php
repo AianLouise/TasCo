438300d40bfc36b0cc99a972f4d8128455b8d481
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-11-15 13:49:14 +0800

    landing page upd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,30 +11,28 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-
         <style>
+            
             ::-webkit-scrollbar{
               display: none;
             }
 
-            
+            /*
             .bg-hero {
                 background-image: url('{{ asset('images/hero-bg.png') }}');
                 height: 100vh;
             }
-              /* Animation for Search Bar */
+            */ 
+            
+            /* Animation for Search Bar */
 
-            ::-webkit-scrollbar{
-              display: none;
-            }
-
-            .searchbox span
-            {
+            .searchbox span{
             position: absolute;
             left: 0%;
-            padding: 15px;
+            padding: 10px 15px;
             pointer-events: none;
             color: #8b8b8b;
             transition: 0.5s;
@@ -44,7 +42,7 @@
             .searchbox input:focus~span
             {
               color: #6698f9;
-              transform: translateX(10px) translateY(-7px);
+              transform: translateX(10px) translateY(-10px);
               font-size: 0.8em;
               padding: 0 10px;
               background: white;
@@ -63,14 +61,14 @@
             }
 
             .AppName:hover::before{
-              color: #322aa3;
+              color: rgb(37 99 235);
               letter-spacing: 0.2em;
             }
 
             /* Animation for Interface */
 
             @keyframes Popdown{
-              from{ 
+              from{
                 transform: translateY(-50px)
               }
               
@@ -81,7 +79,7 @@
             }
 
             @keyframes Popup{
-              0%{ 
+              0%{
                 transform: translateY(200px)
               }
               
@@ -104,7 +102,7 @@
             }
             
             .ImageComp img{
-              display: block; 
+              display: block;
               width: 100%;
               transition: 0.5s ease-in-out;
             }
@@ -113,7 +111,6 @@
               transform: scale(1.3);
               z-index: 2;
             }
-
 
             @keyframes PopLeft{
               from{
@@ -145,10 +142,9 @@
             letter-spacing: 0.1em;
           }
 
-
-
         </style>
     </head>
+    
     <body class="antialiased">
 
         <main class = "relative ">
@@ -170,51 +166,61 @@
                     @endif
                 @endauth
             
-                <div class="w-20 py-5 font-bold text-3xl">
+                <!-- Start: Logo -->
+                <div class="w-20 py-5 font-bold text-2xl">
                     <a href="/">
-                        LOGO
+                        TasCo
                     </a>
                 </div>
-            
-                <div class='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+                <!-- End: Logo -->
+
+                <div class='flex gap-2 leading-normal font-medium max-lg:hidden wide:mr-24'>
                     @auth
                         <!-- No need to display anything here since redirection is done above -->
                     @else
-                        <a href="{{ route('login') }}" class="text-gray-900 hover:text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <a href="{{ route('login') }}" class="text-gray-900 hover:text-gray-500 rounded-md">
                             Log In
                         </a>
-                        <span>|</span>
+                        <span>/</span>
                     @endauth
-                    <a href="{{ route('register') }}" class="text-gray-900 hover:text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Register</a>
+                    <a href="{{ route('register') }}" class="text-gray-900 hover:text-gray-500 rounded-md">Register</a>
                 </div>
             </nav>
             
             </header>
             <section class="xl:padding-1 bg-hero bg-cover wide:padding-r padding-b">
-                <section id="home" class="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
-                  <div class="ContentLeft1 relative xl:w-7/5 flex flex-col justify-center items-start w-full max-xl:padding-x px-9 pt-18">
+            
+            <!-- Start: Left Content--> 
+            <section id="home" class="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+              
+                  <div class="ContentLeft1 relative xl:w-7/5 flex flex-col justify-center items-start max-xl:padding-x px-12 pt-18">
                   
-                    <h1 class="mt-24 sm:mt-10 text-3xl sm:text-5xl max-sm:text-[72px] max-sm:leading-[82px] font-poppins font-bold">
+                    <h1 class="mt-24 sm:mt-10 text-3xl sm:text-5xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
                         <span class="FindLocal xl:whitespace-nowrap relative z-10 pr-10">Find Local Services </span>
                         <br />
-                        with <span class="AppName inline-block mt-6 sm:mt-3 z-10 pr-10"></span>
+                        with <span class="AppName inline-block sm:mt-3 z-10 pr-10"></span>
                     </h1>
                   
-                  
-                  <p class="font-poppins text-slate-gray text-lg leading-8 mt-6 mb-14 sm-w-sm">TasCo is your all-in-one destination for the services you require within your community.</p>
+                  <p class="text-slate-gray text-lg leading-8 mt-6 mb-10 max-sm:text-[72px]max-xl:padding-x">TasCo is your all-in-one destination for the services you require within your community.</p>
                   
                   <form>
                     <div class="ContentLeft2 searchbox relative flex">
-                        <input type="text" name="search" class="flex flex-1 bg-white rounded-md justify-center items-center gap-2 px-5 py-4 sm:w-96 sm:py-3 w-full border-none font-poppins text-lg" required="required">
-                        <span>Search for Services</span>
-                        <button class="Srchbtn flex justify-center items-center gap-2 px-7 ml-1 py-4 border font-poppins font-semibold text-lg leading-none bg-blue rounded-md text-white border-blue">
-                            Search
+                      
+                        <input type="text" name="search" class="flex flex-1 bg-white rounded-md justify-center items-center gap-2 px-5 py-4 sm:w-96 sm:py-3 w-full border text-lg" required="required">
+                        <span>Search for Services</span>              
+                        
+                        <button class="absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">    
+                        <i class="ri-search-line bg-blue-700 mr-1"></i>Search
                         </button>
                     </div>
                   </form>
                 </div>
-                  <div class="ImageComp relative flex justify-center items-center xl:min-h-screen mal-xl:py-40">
-                    <img 
+
+                <!-- End: Left Content-->
+
+                <!-- Start: Right Content-->
+                <div class="ImageComp relative flex justify-center items-center xl:min-h-screen mal-xl:py-40">
+                    <img
                       src={{ URL('images/laptop.png') }}
                       alt="laptop"
                       width={800}
@@ -222,7 +228,8 @@
                       class="object-contain relative z-10"
                     />
                   </div>
-                
+                <!-- End: Right Content-->
+
               </section>
             </section>
             
