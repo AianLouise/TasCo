a93a69b8936cea3ef4b32d53309c13d1253d734f
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-11-15 21:48:47 +0800

    Update user-homepage.blade.php

---
+++ resources/views/user/user-homepage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome to TasCo</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css">
+        @vite(['resources/css/style2.css', 'resources/js/app.js'])
+
+        <style>
+            
+
+        </style>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
