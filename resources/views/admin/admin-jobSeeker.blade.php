4c3fa8fc1acc1a4b8cd0e12f0d0f6ac5276adc6d
AianLouise <aianlouisealfaro03@gmail.com>
2023-11-16 00:26:43 +0800

    Update Admin-jobSeeker
    added confirmation before delete
    update user seeded

--- resources/views/admin/admin-jobSeeker.blade.php
+++ resources/views/admin/admin-jobSeeker.blade.php
@@ -115,12 +115,12 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium2">
                                         <a href="{{ route('admin.editProfile', ['id' => $worker->id]) }}" class="text-blue-400 hover:text-blue-600">Edit</a>
                                         <span class="text-gray-600">/</span>
-                                        <form action="{{ route('admin.deleteProfile', ['id' => $worker->id]) }}" method="POST" class="inline">
+                                        <form action="{{ route('admin.deleteProfile', ['id' => $worker->id]) }}" method="POST" class="inline" onsubmit="return confirm('Are you sure you want to delete this profile?');">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="text-gray-600 hover:text-gray-600">Delete</button>
                                         </form>
-                                    </td>
+                                    </td>                                    
                                 </tr>
                             @endforeach
                         </tbody>                        
