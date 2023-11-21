bf03510627e8491ef5219e9433ed08f9441b2e97
AianLouise <aianlouisealfaro03@gmail.com>
2023-11-21 17:17:20 +0800

    added sorting and search in home

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,6 +20,34 @@ class UserController extends Controller
         return view("user.user-homepage", compact('workerUsers', 'categories'));
     }
 
+    public function sort(Request $request)
+    {
+        $query = User::where('role', 'worker');
+
+        // Category filter logic
+        if ($request->has('category')) {
+            $categoryId = $request->input('category');
+            if ($categoryId !== 'all') {
+                $query->where('category_id', $categoryId);
+            }
+        }
+
+        // Search logic
+        if ($request->has('search')) {
+            $searchTerm = $request->input('search');
+            $query->where('name', 'like', "%$searchTerm%")
+                ->orWhereHas('category', function ($q) use ($searchTerm) {
+                    $q->where('name', 'like', "%$searchTerm%");
+                });
+        }
+
+        // Pagination without sorting
+        $workers = $query->paginate(6);
+        $categories = Category::all();
+
+        return view('user.user-homepage', ['workerUsers' => $workers, 'categories' => $categories]);
+    }
+
     public function UserSettings()
     {
         return view("user.user-settings");
--- resources/views/user/user-homepage.blade.php
+++ resources/views/user/user-homepage.blade.php
@@ -216,6 +216,7 @@
         <!-- End: Roles Section -->
 
         <!-- Start: Workers Section -->
+        <a id="top"></a>
         <section class="relative pt-20 pb-8 md:pt-16 md:pb-0 bg-white">
 
             <div class="container xl:max-w-6xl mx-auto px-4">
@@ -239,14 +240,17 @@
                 </div>
                 <!-- End: Heading -->
 
-
-                <form class="mb-12 w-full">
+                <form class="mb-12 w-full" action="{{ route('workers.sort') }}#top" method="get">
                     <div class="flex">
                         <!-- Your existing button -->
                         <button id="dropdown-button" data-dropdown-toggle="dropdown"
                             class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100"
                             type="button">
-                            All categories
+                            @if (request()->has('category'))
+                                {{ $categories->firstWhere('id', request('category'))->name }}
+                            @else
+                                All categories
+                            @endif
                             <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -256,29 +260,25 @@
 
                         <!-- Dropdown content -->
                         <div id="dropdown-content"
-                            class="hidden absolute z-10 mt-10 bg-white border border-gray-300 rounded-lg shadow-md">
+                            class="hidden absolute z-10 mt-10 bg-white border border-gray-300 rounded-lg shadow-md p-2.5">
+                            <!-- Add "All categories" option -->
+                            <a href="{{ route('workers.sort') }}"
+                                class="block text-sm font-medium text-gray-900 hover:bg-gray-200 p-1">All
+                                categories</a>
                             <!-- Add your dynamic categories here -->
                             @foreach ($categories as $category)
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{{ $category->name }}</a>
+                                <a href="{{ route('workers.sort', ['category' => $category->id]) }}"
+                                    class="block text-sm font-medium text-gray-900 hover:bg-gray-200 p-1">{{ $category->name }}</a>
                             @endforeach
                         </div>
 
-                        <!-- JavaScript to toggle the dropdown -->
-                        <script>
-                            document.getElementById('dropdown-button').addEventListener('click', function() {
-                                document.getElementById('dropdown-content').classList.toggle('hidden');
-                            });
-                        </script>
-
-
                         <div class="relative w-full">
-                            <input type="search" id="search-dropdown"
+                            <input type="search" id="search-dropdown" name="search"
                                 class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                                placeholder="Search for Workers, Employer, Category..." required>
+                                placeholder="Search for Workers, Employer, Category..." required
+                                value="{{ old('search') }}"> <!-- Display old input value -->
                             <button type="submit"
                                 class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-500 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-
                                 <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -333,7 +333,9 @@
 
                     <!-- See More Jobs Button -->
                     <div class="w-full flex justify-center">
-                        <a href="#" class="w-full sm:w-auto bg-blue-500 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5">See More Jobs</a>
+                        <a href="#"
+                            class="w-full sm:w-auto bg-blue-500 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5">See
+                            More Workers</a>
                     </div>
                 </div>
                 <!-- end row -->
