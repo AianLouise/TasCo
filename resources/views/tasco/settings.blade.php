873a2d44ebf5efc1776adbd33542cb8f4e1ea23f
AianLouise <aianlouisealfaro03@gmail.com>
2023-11-25 23:31:51 +0800

    Update settings.blade.php

--- resources/views/tasco/settings.blade.php
+++ resources/views/tasco/settings.blade.php
@@ -39,11 +39,14 @@
         <nav x-data="flex { open: false }" class="bg-white border-b border-gray-100">
             <!-- Start: Sidebar -->
             <div class="fixed top-30 w-64 h-full p-4 z-50 sidebar-menu bg-white">
-                <i id="settings-icon" class="ri-settings-3-fill text-xl text-gray-800"></i>
-                <span class="fixed text-lg font-bold h-full ml-3">Settings</span>
+                <div class="flex justify-between">
+                    <span class="text-lg font-bold">Settings</span>
+                    <i id="settings-icon" class="ri-menu-fold-fill text-xl text-gray-800"></i>
+                </div>
+
                 <!-- Start: Logo -->
                 <div>
-                    <a href="#" class="flex items-center pb-4 border-b mt-8">
+                    <a href="#" class="flex items-center pb-4 border-b">
                     </a>
                 </div>
                 <!-- End: Logo -->
@@ -52,7 +55,7 @@
                 <ul class="mt-4">
 
                     <!-- Information Detail/Dashboard Link -->
-                    <li class="mb-1 group">
+                    <li class="mb-1 group {{ request()->routeIs('app.settings') ? 'active' : '' }}">
                         <a href="{{ route('app.settings') }}"
                             class="flex items-center py-2 px-4 text-gray-800 
                                             hover:bg-blue-400 hover:text-white rounded-md
@@ -126,17 +129,287 @@
         </nav>
     </section>
 
-
-
     
-
-    <div class="h-56 w-3/5 divide-y content-center shadow-lg template">
-        <i id="show-sidebar-icon" class="ri-arrow-left-s-line text-xl text-gray-800"></i>
+    <div class="p-2 mt">
+        <i id="show-sidebar-icon" class="ri-settings-5-fill text-2xl text-gray-800"></i>
+    </div>
+    <div class="bg-white border border-gray-100 shadow-md shadow-black/5 p-5 md:w-1/2 mx-auto w-full mt-4 rounded-md lg:px-4">
         <div class="pl-4 grid grid-cols-1">
+            <span class="titleinfo text-2xl pb-2 pt-2">Personal Information</span>
+        </div>
+        {{-- Name --}}
+        <div>
+            <div
+                class="open-button flex items-center py-2 text-gray-800 
+                hover:bg-blue-400 hover:text-white
+                group-[.active]:bg-blue-600 group-[.active]:text-white
+                group-[.selected]:bg-blue-600 group-[.selected]:text-white">
+                <div class="grid grid-cols-1 pl-4">
+                    <span>Name:</span>
+                    <span>{{ Auth::user()->name }}</span>
+                </div>
+            </div>
+
+            <!-- Updated HTML Structure for First Name and Last Name -->
+            <dialog class="content-center shadow-lg rounded-lg w-96" id="nameModal" style="margin: auto;">
+                <div class="text-center grid grid-rows-6 divide-y divide-gray-200 h-48 mt-4">
+                    <div>
+                        <i class="ri-user-line text-blue-400 text-base"></i>
+                        <span class="tracking-wider font-semibold text-base">Edit Name</span>
+                        <button class="text-3xl close-button">
+                            <i class="ri-close-line absolute top-2 right-2 close-button hover:text-blue-400"></i>
+                        </button>
+                    </div>
+                    <div>
+                        <form class="mt-2 p-4">
+                            <div class="grid grid-cols-2 gap-4">
+                                <input class="rounded-lg w-full p-2 text-base border-gray-200 tracking-wide"
+                                    type="text" id="fname" name="fname" placeholder="First Name"
+                                    value="{{ Auth::user()->first_name }}">
+                                <input class="rounded-lg w-full p-2 text-base border-gray-200 tracking-wide"
+                                    type="text" id="lname" name="lname" placeholder="Last Name"
+                                    value="{{ Auth::user()->last_name }}">
+                            </div>
+                        </form>
+
+                        <button
+                            class="edit-button border rounded-lg border-gray-200 border-solid p-2 mt-2 w-72 text-base
+                                hover:bg-blue-400 hover:text-gray-100 group-[.active]:bg-blue-500 group-[.active]:text-white 
+                                group-[.selected]:bg-blue-600 group-[.selected]:text-gray-100">
+                            <span class="rounded-lg w-full text-base tracking-wide">
+                                <i class="ri-save-line font-bold mr-2 saveButton"></i>Save
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </dialog>
+
+
+            <script>
+                // Add click event listener to open-button
+                const openButton = document.querySelector('.open-button');
+                openButton.addEventListener('click', () => {
+                    const modal = document.getElementById('nameModal');
+                    modal.showModal();
+                });
+
+                // Add click event listener to close-button in View Dialog
+                const closeButton = document.querySelector('.close-button');
+                closeButton.addEventListener('click', () => {
+                    const modal = document.getElementById('nameModal');
+                    modal.close();
+                });
+
+                // Add click event listener to saveButton in Edit Dialog
+                const saveButton = document.querySelector('.saveButton');
+                saveButton.addEventListener('click', () => {
+                    // Add your logic to save the first name and last name
+                    const firstName = document.getElementById('fname').value;
+                    const lastName = document.getElementById('lname').value;
+                    console.log('First Name:', firstName);
+                    console.log('Last Name:', lastName);
+
+                    // Close the dialog after saving
+                    const modal = document.getElementById('nameModal');
+                    modal.close();
+                });
+            </script>
+
+        </div>
+
+        {{-- Address --}}
+        <div>
+            <div
+                class="open-button flex items-center py-2 text-gray-800 
+        hover:bg-blue-400 hover:text-white
+        group-[.active]:bg-blue-600 group-[.active]:text-white
+        group-[.selected]:bg-blue-600 group-[.selected]:text-white">
+                <div class="grid grid-cols-1 pl-4">
+                    <span>Address:</span>
+                    <span>{{ Auth::user()->address }}</span>
+                </div>
+            </div>
+
+            <!-- Updated HTML Structure for Address -->
+            <dialog class="content-center shadow-lg rounded-lg w-96" id="addressModal" style="margin: auto;">
+                <div class="text-center grid grid-rows-6 divide-y divide-gray-200 h-48 mt-4">
+                    <div>
+                        <i class="ri-user-line text-blue-400 text-base"></i>
+                        <span class="tracking-wider font-semibold text-base">Edit Address</span>
+                        <button class="text-3xl close-button">
+                            <i class="ri-close-line absolute top-2 right-2 hover:text-blue-400"></i>
+                        </button>
+                    </div>
+                    <div>
+                        <form class="mt-2 p-4">
+                            <div class="grid gap-4">
+                                <input class="rounded-lg w-full p-2 text-base border-gray-200 tracking-wide"
+                                    type="text" id="address" name="address" placeholder="Address"
+                                    value="{{ Auth::user()->address }}">
+                            </div>
+                        </form>
+
+                        <button
+                            class="edit-button border rounded-lg border-gray-200 border-solid p-2 mt-2 w-72 text-base
+                        hover:bg-blue-400 hover:text-gray-100 group-[.active]:bg-blue-500 group-[.active]:text-white 
+                        group-[.selected]:bg-blue-600 group-[.selected]:text-gray-100 saveButton">
+                            <span class="rounded-lg w-full text-base tracking-wide">
+                                <i class="ri-save-line font-bold mr-2"></i>Save
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </dialog>
+
+            <script>
+                // Add click event listener to open-button for Address
+                const openAddressButton = document.querySelectorAll('.open-button')[1]; // Selecting the second open-button
+                openAddressButton.addEventListener('click', () => {
+                    const modal = document.getElementById('addressModal');
+                    modal.showModal();
+                });
+
+                // Add click event listener to close-button in Address Dialog
+                const closeAddressButton = document.querySelector('#addressModal .close-button');
+                closeAddressButton.addEventListener('click', () => {
+                    const modal = document.getElementById('addressModal');
+                    modal.close();
+                });
+            </script>
+        </div>
+
+        {{-- Email --}}
+        <div>
+            <div
+                class="open-button flex items-center py-2 text-gray-800 
+        hover:bg-blue-400 hover:text-white
+        group-[.active]:bg-blue-600 group-[.active]:text-white
+        group-[.selected]:bg-blue-600 group-[.selected]:text-white">
+                <div class="grid grid-cols-1 pl-4">
+                    <span>Email:</span>
+                    <span>{{ Auth::user()->email }}</span>
+                </div>
+            </div>
+
+            <!-- Updated HTML Structure for Phone -->
+            <dialog class="content-center shadow-lg rounded-lg w-96" id="emailModal" style="margin: auto;">
+                <div class="text-center grid grid-rows-6 divide-y divide-gray-200 h-48 mt-4">
+                    <div>
+                        <i class="ri-user-line text-blue-400 text-base"></i>
+                        <span class="tracking-wider font-semibold text-base">Edit Email</span>
+                        <button class="text-3xl close-button">
+                            <i class="ri-close-line absolute top-2 right-2 hover:text-blue-400"></i>
+                        </button>
+                    </div>
+                    <div>
+                        <form class="mt-2 p-4">
+                            <div class="grid gap-4">
+                                <input class="rounded-lg w-full p-2 text-base border-gray-200 tracking-wide"
+                                    type="text" id="email" name="email" placeholder="Email"
+                                    value="{{ Auth::user()->email }}">
+                            </div>
+                        </form>
+
+                        <button
+                            class="edit-button border rounded-lg border-gray-200 border-solid p-2 mt-2 w-72 text-base
+                    hover:bg-blue-400 hover:text-gray-100 group-[.active]:bg-blue-500 group-[.active]:text-white 
+                    group-[.selected]:bg-blue-600 group-[.selected]:text-gray-100 saveButton">
+                            <span class="rounded-lg w-full text-base tracking-wide">
+                                <i class="ri-save-line font-bold mr-2"></i>Save
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </dialog>
+
+            <script>
+                // Add click event listener to open-button for Email
+                const openEmailButton = document.querySelectorAll('.open-button')[2]; // Selecting the third open-button
+                openEmailButton.addEventListener('click', () => {
+                    const modal = document.getElementById('emailModal');
+                    modal.showModal();
+                });
+
+                // Add click event listener to close-button in Phone Dialog
+                const closeEmailButton = document.querySelector('#emailModal .close-button');
+                closeEmailButton.addEventListener('click', () => {
+                    const modal = document.getElementById('emailModal'); 
+                    modal.close();
+                });
+            </script>
+        </div>
+
+        {{-- Phone --}}
+        <div>
+            <div
+                class="open-button flex items-center py-2 text-gray-800 
+        hover:bg-blue-400 hover:text-white
+        group-[.active]:bg-blue-600 group-[.active]:text-white
+        group-[.selected]:bg-blue-600 group-[.selected]:text-white">
+                <div class="grid grid-cols-1 pl-4">
+                    <span>Phone:</span>
+                    <span>{{ Auth::user()->phone }}</span>
+                </div>
+            </div>
+
+            <!-- Updated HTML Structure for Phone -->
+            <dialog class="content-center shadow-lg rounded-lg w-96" id="phoneModal" style="margin: auto;">
+                <div class="text-center grid grid-rows-6 divide-y divide-gray-200 h-48 mt-4">
+                    <div>
+                        <i class="ri-user-line text-blue-400 text-base"></i>
+                        <span class="tracking-wider font-semibold text-base">Edit Phone</span>
+                        <button class="text-3xl close-button">
+                            <i class="ri-close-line absolute top-2 right-2 hover:text-blue-400"></i>
+                        </button>
+                    </div>
+                    <div>
+                        <form class="mt-2 p-4">
+                            <div class="grid gap-4">
+                                <input class="rounded-lg w-full p-2 text-base border-gray-200 tracking-wide"
+                                    type="text" id="phone" name="phone" placeholder="Phone"
+                                    value="{{ Auth::user()->phone }}">
+                            </div>
+                        </form>
+
+                        <button
+                            class="edit-button border rounded-lg border-gray-200 border-solid p-2 mt-2 w-72 text-base
+                    hover:bg-blue-400 hover:text-gray-100 group-[.active]:bg-blue-500 group-[.active]:text-white 
+                    group-[.selected]:bg-blue-600 group-[.selected]:text-gray-100 saveButton">
+                            <span class="rounded-lg w-full text-base tracking-wide">
+                                <i class="ri-save-line font-bold mr-2"></i>Save
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </dialog>
+
+            <script>
+                // Add click event listener to open-button for Phone
+                const openPhoneButton = document.querySelectorAll('.open-button')[3]; // Selecting the third open-button
+                openPhoneButton.addEventListener('click', () => {
+                    const modal = document.getElementById('phoneModal');
+                    modal.showModal();
+                });
+
+                // Add click event listener to close-button in Phone Dialog
+                const closePhoneButton = document.querySelector('#phoneModal .close-button');
+                closePhoneButton.addEventListener('click', () => {
+                    const modal = document.getElementById('phoneModal');
+                    modal.close();
+                });
+            </script>
+        </div>
+    </div>
+
+
+
+
+
+
+    {{-- <div class="pl-4 grid grid-cols-1">
             <span class="titleinfo text-2xl pb-2 pt-2">Contact Info</span>
         </div>
 
-        {{-- EMAIL --}}
         <div>
             <div
                 class="open-button flex items-center py-2 text-gray-800 
@@ -214,7 +487,7 @@
             </dialog>
         </div>
 
-        {{-- END OF EMAIL --}}
+
         <div
             class="open-button flex items-center py-2 text-gray-800 
             hover:bg-blue-400 hover:text-white
@@ -290,7 +563,7 @@
             <span>example</span>
         </dialog>
     </div>
-    </div>
+    </div> --}}
 
     {{-- !! PASSWORD AND SECURITY --}}
 
@@ -380,7 +653,7 @@
 
     {{-- SCRIPT FOR MODALS/POP UP --}}
 
-    <script>
+    {{-- <script>
         // Email
         const modal = document.querySelector('#modal');
         const openModal = document.querySelector('.open-button');
@@ -418,7 +691,7 @@
             verifyModal.close();
         })
         // Email
-    </script>
+    </script> --}}
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
