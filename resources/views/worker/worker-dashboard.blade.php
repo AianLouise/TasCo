10e43dc83c3fc20e83aed181028276e6bf33827b
AianLouise <aianlouisealfaro03@gmail.com>
2023-12-10 11:31:16 +0800

    Update worker-dashboard.blade.php

--- resources/views/worker/worker-dashboard.blade.php
+++ resources/views/worker/worker-dashboard.blade.php
@@ -663,7 +663,7 @@
                                             </div>
                                         @endforeach
                                         <div>
-                                            <a href="{{ route('worker.MarkAsCompleted' , ) }}"
+                                            <a href="{{ route('worker.MarkAsCompleted') }}"
                                                 class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                                 Confirm Mark as Completed
                                             </a>
