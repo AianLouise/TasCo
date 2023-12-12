81e701fa7850ba659dd3fc54d06667e00ff70ce7
Repxl <jrreyas@gmail.com>
2023-12-12 21:23:33 +0800

    Update profile.blade.php

--- resources/views/tasco/profile.blade.php
+++ resources/views/tasco/profile.blade.php
@@ -74,7 +74,8 @@
                             Message
                         </a>
 
-                        <a href="{{ route('app.employments', ['worker' => $worker->id]) }}"
+                        {{-- <a href="{{ route('app.employments', ['worker' => $worker->id]) }}" --}}
+                            <a href="['worker' => $worker->id]) }}"
                             class="text-sm font-semibold bg-white border-blue-500 border border-solid hover:bg-blue-500 hover:text-white text-black py-2 px-4 rounded w-36 max-h-10">Employments</a>
                     </div>
                 </div>
