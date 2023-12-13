a0d0ff60821e2c6d653084016f844454d026780a
AianLouise <aianlouisealfaro03@gmail.com>
2023-12-13 13:01:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/style2.css',
                 'resources/js/app.js',
+                'resources/js/script.js',
             ],
             refresh: true,
         }),
